0136d63900c331275d412d8470b6dee0fa103f9f
Mohammed Alam <ma3655@rit.edu>
2021-06-20 22:51:38 -0400

    new calc design

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Python_Projects
-Some Python Projects I make for fun, and to increase my knowledge of Python with. 
+
+Some Python Projects I make for fun, and to increase my knowledge of Python with.
+
+One of my favorite Projects was the webapp where
+
+- I learned to make a flask server usign the Raspberry Pi Guides to get the structure for submittign a practice webpage online
+- I made use of the Flask Library in Python submitting HTML & CSS files to build a site.
+
+
+Another favorite of Mine is the tKinterExamples where I learned the whole tKinter Python Library by myself and created some projects to reinforce that learning
+
+- One of ym projects was a calculator similar to my Java GUI material, where I configured rows and cols to design a simple calculator to process and input user data, but with numbers.
--- tKinterExample/calculator.py
+++ tKinterExample/calculator.py
@@ -3,8 +3,8 @@ from tkinter import *
 root = Tk()
 root.title("Simple Calculator")
 
-e = Entry(root, width=35, borderwidth=5)
-e.grid(row=0,column=0,columnspan=3, padx=10, pady=10)
+e = Entry(root, width=50, borderwidth=5)
+e.grid(row=0,column=0,columnspan=4, padx=10, pady=10)
 
 def button_click(number):
     #Gets the current value inside and stores it in current
@@ -23,7 +23,7 @@ def button_adder():
     global f_num
     global math_state
     math_state = 'addition'
-    f_num = int(first_number)
+    f_num = float(first_number)
     e.delete(0,END)
 
 def button_subtracter():
@@ -32,7 +32,7 @@ def button_subtracter():
     global f_num
     global math_state
     math_state = 'subtraction'
-    f_num = int(first_number)
+    f_num = float(first_number)
     e.delete(0,END)
 
 def button_multiplier():
@@ -41,7 +41,7 @@ def button_multiplier():
     global f_num
     global math_state
     math_state = 'multiplication'
-    f_num = int(first_number)
+    f_num = float(first_number)
     e.delete(0,END)
 
 def button_divider():
@@ -50,7 +50,7 @@ def button_divider():
     global f_num
     global math_state
     math_state = 'division'
-    f_num = int(first_number)
+    f_num = float(first_number)
     e.delete(0,END)
 
 def button_equal():
@@ -58,13 +58,13 @@ def button_equal():
     e.delete(0, END)
 
     if(math_state == "addition"):
-        e.insert(0, f_num + int(second_number))
+        e.insert(0, f_num + float(second_number))
     elif(math_state == "subtraction"):
-        e.insert(0, f_num - int(second_number))
+        e.insert(0, f_num - float(second_number))
     elif(math_state == "multiplication"):
-        e.insert(0, f_num * int(second_number))
+        e.insert(0, f_num * float(second_number))
     elif(math_state == "division"):
-        e.insert(0, f_num / int(second_number))
+        e.insert(0, f_num / float(second_number))
 
 
 #Define the buttons
@@ -78,15 +78,19 @@ button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click
 button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
-button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
+button_0 = Button(root, text="0", padx=88, pady=20, command=lambda: button_click(0))
+
+button_dot = Button(root, text=".", padx=41, pady=20, command=lambda: button_click("."))
+
+button_clear = Button(root, text="Clear", padx=78, pady=20, command=button_clear)
 
 button_adder = Button(root, text="+", padx=39, pady=20, command=button_adder)
-button_equal = Button(root, text="=", padx=91, pady=20, command=button_equal)
-button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
-
 button_subtracter = Button(root, text="-", padx=41, pady=20, command=button_subtracter)
 button_multiplier = Button(root, text="*", padx=40, pady=20, command=button_multiplier)
 button_divider = Button(root, text="/", padx=40, pady=20, command=button_divider)
+
+button_equal = Button(root, text="=", padx=88, pady=20, command=button_equal)
+
 
 #putting the buttons on the screen
 button_1.grid(row=3, column=0)
@@ -101,14 +105,16 @@ button_7.grid(row=1, column=0)
 button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
-button_0.grid(row=4, column=0)
-button_clear.grid(row=4, column=1, columnspan=2)
-button_adder.grid(row=5, column=0)
-button_equal.grid(row=5, column=1, columnspan=2)
+button_0.grid(row=4, column=0, columnspan=2)
+button_dot.grid(row=4, column=2)
 
-button_subtracter.grid(row=6, column=0)
-button_multiplier.grid(row=6, column=1)
-button_divider.grid(row=6, column=2)
+button_clear.grid(row=5, column=0, columnspan=2)
 
+button_adder.grid(row=1, column=3)
+button_subtracter.grid(row=2, column=3)
+button_multiplier.grid(row=3, column=3)
+button_divider.grid(row=4, column=3)
+
+button_equal.grid(row=5, column=2, columnspan=3)
 
 root.mainloop()
