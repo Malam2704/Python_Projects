f1fd844a333b9604051f2d4cf677d3829915092f
Mohammed Alam <ma3655@rit.edu>
2021-06-21 22:01:01 -0400

    Made a ew mdoule that displays image # we are currently on

--- tKinterExample/status.py
+++ tKinterExample/status.py
@@ -14,6 +14,8 @@ my_img5 = ImageTk.PhotoImage(Image.open("tKinterExample/images/oldnyc.jpg"))
 
 image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 
+status = Label(root, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
 
@@ -36,6 +38,10 @@ def forward(image_number):
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
+    # Update Status Bar
+    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
 def back(image_number):
     global my_label
     global button_forward
@@ -54,12 +60,17 @@ def back(image_number):
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
+    # Update Status Bar
+    status = Label(root, text="Image " + str(image_number) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
 button_back = Button(root, text="<<", state=DISABLED)
 button_quit = Button(root, text="EXIT PROGRAM", command=root.quit)
 button_forward = Button(root, text=">>", command=lambda: forward(2))
 
 button_back.grid(row=1, column=0)
 button_quit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
+button_forward.grid(row=1, column=2, pady=10)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 root.mainloop()
