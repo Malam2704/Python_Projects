0fce7247fe1f8b8f3188d9517ee089cf34e31c73
Mohammed Alam <ma3655@rit.edu>
2021-06-28 18:19:01 -0400

    Slider functions added

--- tKinterExample/slider.py
+++ tKinterExample/slider.py
@@ -7,17 +7,17 @@ root.title("Images Practice")
 root.iconbitmap('tKinterExample/images/flamesword.ico')
 root.geometry("400x400")
 
-vertical = Scale(root, from_=0, to=200)
+def slide():
+    myLabel = Label(root,text=horizontal.get()).pack()
+    root.geometry(str(horizontal.get() * 2) + "x" + str(vertical.get() * 2))
+    
+vertical = Scale(root, from_=0, to=400)
 vertical.pack()
 
-horizontal = Scale(root, from_=0, to=200, orient=HORIZONTAL)
+horizontal = Scale(root, from_=0, to=400, orient=HORIZONTAL)
 horizontal.pack()
 
 myLabel = Label(root,text=horizontal.get()).pack()
-
-def slide():
-    myLabel = Label(root,text=horizontal.get()).pack()
-    root.geometry(str(horizontal.get() * 2) + "x" + str(horizontal.get() * 2))
 
 my_btn = Button(root, text="Click Me!", command=slide).pack()
 
