73994fa54d3153a79e8da7ae9384afadfc9b14ad
Mohammed Alam <ma3655@rit.edu>
2024-12-28 09:43:06 -0500

    project status all good

--- PytonWeekOfFun/ToDoCLI/Runner.py
+++ PytonWeekOfFun/ToDoCLI/Runner.py
@@ -4,29 +4,29 @@ def print_csv_data():
     with open('data.csv', 'r') as file:
         csvFile = csv.reader(file)
         for lines in csvFile:
-            print(lines[0],lines[1])
+            print("   ",lines[0],lines[1])
 
 def add_task():
-    with open('data.csv', 'w', newline="") as file:
+    with open('data.csv', 'a', newline="") as file:
         writer = csv.writer(file)
-        field = ["name","age","country"]
+        
         while(True):
-            
-            choice = input("Here are your tasks, type 'new' for new task, or exit to leave: ")
-            
-            if(choice == "exit"):
-                break
-            elif(choice == "new"):
-                user_date = input("Enter the date for your task: ")
-                user_tasks = input("Enter your tasks you will complete: ")
+            print("Current Dates & Tasks, (Recently added not shown until exit)")
+            print_csv_data()
+            user_date = input("Enter the date for your task: ")
+            user_tasks = input("Enter your tasks you will complete: ")
+
+            if(user_date == 'exit' or user_tasks =='exit'):
+                break    
+            else:
                 writer.writerow([user_date,user_tasks])
 
 
 def main():
     while(True):
-        print("Current Dates")
+        print("Current Dates & Tasks")
         print_csv_data()
-        user_answer = input("What would you like to do?\n add - User can add a new item to their list\n exit - Leaves the application\n")
+        user_answer = input("What would you like to do?\n    add - User can add a new item to their list\n    exit - Leaves the application\n")
 
         if(user_answer == "exit"):
             break
