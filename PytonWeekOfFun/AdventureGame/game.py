84c8374b88c915f7fc9a748f1352b7c92bc63a03
Mohammed Alam <ma3655@rit.edu>
2025-01-02 23:59:24 -0500

    Buying items from shop and inventory feature

--- PytonWeekOfFun/AdventureGame/game.py
+++ PytonWeekOfFun/AdventureGame/game.py
@@ -5,6 +5,13 @@ class Player():
         self.gold = gold
         self.current_item = "Basic Sword"
         self.current_armor = "Basic Armor"
+        self.inventory = []
+
+class Item():
+    def __init__(self, name, attack_power, price):
+        self.name = name
+        self.attack_power = attack_power
+        self.price = price
 
 def print_characters(characters):
     print("\n----------Current Characters----------")
@@ -13,9 +20,14 @@ def print_characters(characters):
     print('\n')
 
 def main():
-    characters = set()
-    shop_items = {"Basic Sword": 5, "Bronze Sword": 15, "Silver Sword": 50, "Gold Sword": 100}
     playing = input("\n*********** Welcome to Treasure Quest! ***********\n\t Type 'Play' to Enter\n")
+
+    characters = {}
+    shop_items = {"Basic Sword": Item("Basic Sword", 5, 5), 
+                  "Bronze Sword": Item("Bronze Sword", 15, 15), 
+                  "Silver Sword": Item("Silver Sword", 50, 50), 
+                  "Gold Sword": Item("Gold Sword", 100, 100)}
+    
     chosen_character = None
     
     while(chosen_character == None):
@@ -24,7 +36,7 @@ def main():
         
         if(len(characters) == 0):
             character_name = input("Name your character: ")
-            characters.add(Player(character_name))
+            characters[character_name] = Player(character_name)
         else:
             
             print_characters(characters)
@@ -33,21 +45,37 @@ def main():
 
             if(creating_new_character == 'create'):
                 character_name = input("Name your character: ")
-                characters.add(Player(character_name))
+                characters[character_name] = Player(character_name)
             elif(creating_new_character == 'continue'):
                 chosen_character = input("Choose a character: ")
-                for character in characters:
-                    if(chosen_character.lower() == (character.name).lower()):
-                        print("Let us Continue " + character.name + "\n")
+                if chosen_character in characters:
+                    print("Let us Continue " + chosen_character + "\n")
+                    chosen_character = characters[chosen_character]
     
     while(True):
-        player_action = input("****** Player Home *****\n -- Shop\n -- Fight -- Exit Game (Type 'exit'\n")
+        player_action = input("****** Player Home *****\n -- Shop\n -- Fight\n -- Exit Game (Type 'exit')\n")
 
         if(player_action.lower() == 'shop'):
             print("\n ***** Shop Items *****")
             for item in shop_items:
-                print(f"{item}, Attack Power: {shop_items[item]}")
-            print("\n")
+                print(f"\t{item}, Attack Power: {shop_items[item].attack_power}, Price: {shop_items[item].price}")
+            player_shop_action = input("What will you do?\n Buy\n Exit: " )
+
+            if(player_shop_action.lower() == 'exit'):
+                continue
+            elif(player_shop_action.lower() == 'buy'):
+                buying_choice = input('type in item to buy: ')
+                if buying_choice in shop_items:
+                    if(chosen_character.gold > shop_items[buying_choice].price):
+                        print("You obtained " + buying_choice)
+                        chosen_character.gold -= shop_items[buying_choice].price
+                        chosen_character.inventory.append(shop_items[buying_choice])
+                    else:
+                        print("You cannot afford this item")
+        elif(player_action.lower() == 'inventory'):
+            print("\n ***** Shop Items *****")
+            for item in chosen_character.inventory:
+                print(item)
         if(player_action.lower() == 'exit'):
             break
 
