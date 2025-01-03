36e6b2907c7d75afc82095a4cb545e54a259803d
Mohammed Alam <ma3655@rit.edu>
2025-01-02 23:13:56 -0500

    Character Creation Complete

--- PytonWeekOfFun/AdventureGame/game.py
+++ PytonWeekOfFun/AdventureGame/game.py
@@ -3,15 +3,21 @@ class Player():
         self.name = name
         self.health = health
         self.gold = gold
+        self.current_item = "Basic Sword"
+        self.current_armor = "Basic Armor"
 
 def print_characters(characters):
+    print("\n----------Current Characters----------")
     for character in characters:
         print(character.name)
+    print('\n')
 
 def main():
     characters = set()
-    playing = input("Welcome to Treasure Quest!\n Type Play to Enter\n")
-    while(True):
+    playing = input("Welcome to Treasure Quest!\n Type 'Play' to Enter\n")
+    chosen_character = None
+    
+    while(chosen_character == None):
         if(playing.lower() != "play"):
             break
         
@@ -19,9 +25,10 @@ def main():
             character_name = input("Name your character: ")
             characters.add(Player(character_name))
         else:
-            print("Current Characters")
+            
             print_characters(characters)
-            creating_new_character = input("type 'create' to make a new character or 'continue' to resume Player journey")
+            
+            creating_new_character = input("type 'create' to make a new character or 'continue' to resume Player journey: ")
 
             if(creating_new_character == 'create'):
                 character_name = input("Name your character: ")
@@ -29,7 +36,10 @@ def main():
             elif(creating_new_character == 'continue'):
                 chosen_character = input("Choose a character: ")
                 for character in characters:
-                    if(chosen_character.lower() == character.name):
+                    if(chosen_character.lower() == (character.name).lower()):
                         print("Let us Continue " + character.name)
+    
+    while(True):
+
 
 main()
