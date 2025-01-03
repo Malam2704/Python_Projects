4baae2fda3b3d4fe33db26c298bda7561b47ed7c
Mohammed Alam <ma3655@rit.edu>
2025-01-03 00:21:19 -0500

    Buying works & Inventory exists

--- PytonWeekOfFun/AdventureGame/game.py
+++ PytonWeekOfFun/AdventureGame/game.py
@@ -14,13 +14,12 @@ class Item():
         self.price = price
 
 def print_characters(characters):
-    print("\n----------Current Characters----------")
+    print("---------- Current Characters ----------")
     for character in characters:
-        print(character.name)
-    print('\n')
+        print(character)
 
 def main():
-    playing = input("\n*********** Welcome to Treasure Quest! ***********\n\t Type 'Play' to Enter\n")
+    playing = input("\n*********** Welcome to Treasure Quest! ***********\n\t Type 'Play' to Enter\n:")
 
     characters = {}
     shop_items = {"Basic Sword": Item("Basic Sword", 5, 5), 
@@ -37,45 +36,47 @@ def main():
         if(len(characters) == 0):
             character_name = input("Name your character: ")
             characters[character_name] = Player(character_name)
+            chosen_character = characters[character_name]
         else:
-            
             print_characters(characters)
-            
-            creating_new_character = input("type 'create' to make a new character or 'continue' to resume Player journey: ")
-
+            creating_new_character = input("Type 'create' to make a new character or enter character name: ")
             if(creating_new_character == 'create'):
                 character_name = input("Name your character: ")
                 characters[character_name] = Player(character_name)
-            elif(creating_new_character == 'continue'):
-                chosen_character = input("Choose a character: ")
-                if chosen_character in characters:
-                    print("Let us Continue " + chosen_character + "\n")
-                    chosen_character = characters[chosen_character]
+            else:
+                if creating_new_character in characters:
+                    print("Let us Continue " + creating_new_character + "\n")
+                    chosen_character = characters[creating_new_character]
+                else:
+                    print("!!!!! Character not found !!!!!")
     
-    while(True):
-        player_action = input("****** Player Home *****\n -- Shop\n -- Fight\n -- Exit Game (Type 'exit')\n")
+    while(playing.lower() == "play"):
+        player_action = input("****** Player Home *****\n -- Shop\n -- Fight\n -- Inventory\n -- Exit Game (Type 'exit')\n:")
 
         if(player_action.lower() == 'shop'):
-            print("\n ***** Shop Items *****")
+            print("\n  ***** Shop Items *****")
             for item in shop_items:
                 print(f"\t{item}, Attack Power: {shop_items[item].attack_power}, Price: {shop_items[item].price}")
-            player_shop_action = input("What will you do?\n Buy\n Exit: " )
+            player_shop_action = input("  What will you do?\n    Buy\n    Exit\n:" )
 
             if(player_shop_action.lower() == 'exit'):
                 continue
             elif(player_shop_action.lower() == 'buy'):
                 buying_choice = input('type in item to buy: ')
                 if buying_choice in shop_items:
-                    if(chosen_character.gold > shop_items[buying_choice].price):
-                        print("You obtained " + buying_choice)
+                    if(chosen_character.gold >= shop_items[buying_choice].price):
                         chosen_character.gold -= shop_items[buying_choice].price
                         chosen_character.inventory.append(shop_items[buying_choice])
+                        print("You obtained " + buying_choice)
                     else:
                         print("You cannot afford this item")
+                else:
+                    print("Item does not exist")
         elif(player_action.lower() == 'inventory'):
-            print("\n ***** Shop Items *****")
+            print("\n  ***** Inventory Items *****")
             for item in chosen_character.inventory:
-                print(item)
+                print(f"{item.name}")
+            print("\n")
         if(player_action.lower() == 'exit'):
             break
 
