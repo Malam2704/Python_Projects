9a8a66c5d708ae89c268e3c937b4a8fbbfb35a0a
Mohammed Alam <ma3655@rit.edu>
2025-01-08 19:24:31 -0500

    bot updates

--- PythonWeekOfFun/dicordbot/bot.py
+++ PythonWeekOfFun/dicordbot/bot.py
@@ -1,8 +1,7 @@
 from discord.ext import commands
 import discord
 
-BOT_TOKEN = 'MTMyNTUxMTI5MDgwMTg4MTA5OA.GUQQ76.niUfDeeYkhe_7CkvQFQvuPhS50Bt4_AZpJcHU0'
-Channel_ID = 1325513357935120384
+
 
 bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
