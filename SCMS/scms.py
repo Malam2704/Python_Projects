2d41ee2fc4550c74e4b921bb5836bf39e395107b
Mohammed Alam <ma3655@rit.edu>
2025-03-08 18:12:54 -0500

    classified total storage

--- SCMS/scms.py
+++ SCMS/scms.py
@@ -1,8 +1,18 @@
-total_storage = {}
+class storage_system():
+    def __init__(self):
+        self.my_items = {}
 
-def print_storage():
-    for i in list(total_storage):
-        print(total_storage[i])
+    def add_item(self, Product, quantity_added=0):
+        if(self.my_items[Product.id]):
+            self.my_items[Product.id] = self.my_items[Product.id].change_quantity(quantity_added)
+        else:
+            Product.change_quantity(quantity_added)
+            self.my_items[Product.id] = Product
+
+    def display_items(self):
+        for i in list(self.my_items):
+            print(self.my_items[i])
+
 
 class Product:
     def __init__(self, id, name, quantity = 0):
@@ -12,12 +22,14 @@ class Product:
 
     def __str__(self):
         return f" ID: {self.id}\n Name: {self.name} \n Quantity: {self.quantity}\n"
+    
+    def change_quantity(self, new_quantity):
+        self.quantity = new_quantity
 
 def main():
     new_product = Product(12345, "Tablets", 55)
-    total_storage[new_product.id] = new_product
-
-    print_storage()
+    total_storage = storage_system()
+    total_storage.add_item(new_product)
 
 if __name__ == "__main__":
     main()
